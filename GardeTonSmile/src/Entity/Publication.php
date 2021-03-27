6d61df6640402e35f51305284ba4f2386b04255d
DilouProd <moreira.dylan@hotmail.fr>
2021-03-27 12:06:49 +0100

    Add bootstrap & use form themes & fix little bug with setteers

--- GardeTonSmile/src/Entity/Publication.php
+++ GardeTonSmile/src/Entity/Publication.php
@@ -46,7 +46,7 @@ class Publication
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -58,7 +58,7 @@ class Publication
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
