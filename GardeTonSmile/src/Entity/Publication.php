7e4465c08fbc80f8a54238964c0cbc5d6c7e65bb
DilouProd <moreira.dylan@hotmail.fr>
2021-03-30 22:39:33 +0200

    Add validation for image size

--- GardeTonSmile/src/Entity/Publication.php
+++ GardeTonSmile/src/Entity/Publication.php
@@ -43,6 +43,7 @@ class Publication
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      * 
      * @Vich\UploadableField(mapping="publication_image", fileNameProperty="imageName")
+     * @Assert\Image(maxSize="8M")
      * 
      * @var File|null
      */
