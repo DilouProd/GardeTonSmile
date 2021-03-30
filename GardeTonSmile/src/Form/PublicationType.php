a9e872793ab46c7f54fd6c8450e6582c0a89829a
DilouProd <moreira.dylan@hotmail.fr>
2021-03-30 21:16:03 +0200

    Image upload works!

--- GardeTonSmile/src/Form/PublicationType.php
+++ GardeTonSmile/src/Form/PublicationType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\Publication;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PublicationType extends AbstractType
@@ -13,8 +13,14 @@ class PublicationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
+        ->add('imageFile', VichImageType::class, [
+            'label' => 'Image (JPG ou PNG file)',
+            'required' => false,
+            'allow_delete' => false,
+            'asset_helper' => true,
+        ])
+        ->add('title')
+        ->add('description')
         ;
     }
 
