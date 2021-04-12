d1eef503f616a4684377846028e81d81d34a96c0
DilouProd <moreira.dylan@hotmail.fr>
2021-04-12 09:15:12 +0200

    Refactoring of SCSS code and better name for puraliser twig extension method

--- GardeTonSmile/src/Twig/AppExtension.php
+++ GardeTonSmile/src/Twig/AppExtension.php
@@ -3,29 +3,19 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
-    public function getFilters(): array
-    {
-        return [
-            // If your filter generates SAFE HTML, you should add a third
-            // parameter: ['is_safe' => ['html']]
-            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
-        ];
-    }
 
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('puraliser', [$this, 'doSomething']),
+            new TwigFunction('puraliser', [$this, 'puraliser']),
         ];
     }
 
-    public function doSomething(int $compte, string $singulier, ?string $pluriel = null): string
+    public function puraliser(int $compte, string $singulier, ?string $pluriel = null): string
     {
         $pluriel = $pluriel ?? $singulier . 's';
         $resultat = $compte === 1 ? $singulier : $pluriel;
