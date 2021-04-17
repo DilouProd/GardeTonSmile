98c6404bb98f7617e178009df1ac96bc6773ecd4
DilouProd <moreira.dylan@hotmail.fr>
2021-04-17 14:49:41 +0200

    explicit is better than implicit

--- GardeTonSmile/src/Form/RegistrationFormType.php
+++ GardeTonSmile/src/Form/RegistrationFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,9 +19,9 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('firstName')
-        ->add('lastName')
-        ->add('email')
+        ->add('firstName', TextType::class)
+        ->add('lastName', TextType::class)
+        ->add('email', EmailType::class)
         ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
